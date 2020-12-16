a376371a1ace8f512f47adfb832e87efc54eff89
AlejandroGs13 <alejandrogs13@outlook.com>
2020-12-16 17:46:58 -0600

    clase Static Platzo

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import static UI.UIMenu.*;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -5,10 +7,12 @@ public class Main {
         myDoctor.setName("Alejandro");
         myDoctor.showId();
         Doctor myDoctor2 = new Doctor();
-        myDoctor.setName("Alejandro");
+        myDoctor.setName("David");
         myDoctor.showId();
         Doctor myDoctor3 = new Doctor();
-        myDoctor.setName("Alejandro");
+        myDoctor.setName("garcia");
         myDoctor.showId();
+
+        showMenu();
     }
 }
