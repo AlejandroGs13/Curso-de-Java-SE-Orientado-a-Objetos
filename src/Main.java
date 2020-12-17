39642f4bd7316dbde398beec1fb54fcb4acf4f36
AlejandroGs13 <alejandrogs13@outlook.com>
2020-12-16 18:15:58 -0600

    test shh

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,7 @@ import static UI.UIMenu.*;
 public class Main {
 
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
-        myDoctor.setName("Alejandro");
-        myDoctor.showId();
-        Doctor myDoctor2 = new Doctor();
-        myDoctor.setName("David");
-        myDoctor.showId();
-        Doctor myDoctor3 = new Doctor();
-        myDoctor.setName("garcia");
-        myDoctor.showId();
+
 
         showMenu();
     }
